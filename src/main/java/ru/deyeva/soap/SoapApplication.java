19f94c448808c7470baa5c89c963bc66dfcd56b3
tdeeva <tadeeva@dev.vtb.ru>
2023-11-06 15:39:54 +0300

    -

--- src/main/java/ru/deyeva/soap/SoapApplication.java
+++ src/main/java/ru/deyeva/soap/SoapApplication.java
@@ -1,7 +1,6 @@
 package ru.deyeva.soap;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class SoapApplication {
 
